2b635185cdce124d3d42799f50ff7242465226d7
juliosap <juliosaputro307@gmail.com>
2022-09-28 01:31:05 +0700

    delete button

--- src/components/table/TableData.js
+++ src/components/table/TableData.js
@@ -18,8 +18,12 @@ import {
     TableRow,
     Paper,
     Box,
+    Button,
+    Stack
 } from '@mui/material'
-import { uuidv4 } from '@firebase/util';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModeIcon from '@mui/icons-material/Mode';
+
 
 
 export default function TableData() {
@@ -31,6 +35,12 @@ export default function TableData() {
     // });
     const [kas, setKas] = useState([]);
 
+  const deleteDocument = async (id) => {
+    const kasDoc = doc(db,"kas", id);
+    await deleteDoc(kasDoc)
+    console.log('coba delete')
+    window.location.reload(true)
+  }
 
     
     useEffect(() => {
@@ -59,16 +69,17 @@ export default function TableData() {
           <Table aria-label="simple table">
             <TableHead>
               <TableRow>
-                {/* <TableCell align="center">No</TableCell> */}
+                <TableCell align="center">No</TableCell>
                 <TableCell align="center">Tanggal</TableCell>
                 <TableCell align="center">Pemasukan</TableCell>
                 <TableCell align="center">Pengeluaran</TableCell>
                 <TableCell align="center">Keterangan</TableCell>
                 <TableCell align="center">Saldo (Mingguan)</TableCell>
+                <TableCell align="center">Aksi</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {kas.map((k) => {
+              {kas.map((k,y) => {
                 // console.log(nikahs,'data')
                 const tgl = new Date(
                   k.tanggal.seconds * 1000 +
@@ -77,17 +88,35 @@ export default function TableData() {
                 const tanggal = tgl.getDate()
                 const month = tgl.getMonth()
                 const year = tgl.getFullYear()
+                const id_data = y + 1;
+                function delete_data () {
+                  deleteDoc(doc(db, "kas", id_data ))
+                  window.location.reload(true)
+                }
                 
                 return (
                 <TableRow key={k.id}>
-                    {/* <TableCell align="center">(0++)</TableCell> */}
+                    <TableCell align="center">{id_data}</TableCell>
                     <TableCell align="center">{tanggal}/{month}/{year}</TableCell>
                     <TableCell align="center">{rupiah(k.pemasukan)}</TableCell>
                     <TableCell align="center">{rupiah(k.pengeluaran)}</TableCell>
                     <TableCell align="center">{k.keterangan}</TableCell>
                     <TableCell align="center">{rupiah(k.saldo)}</TableCell>
-                    {/* <TableCell align="center">{nikah.no_examp}</TableCell>
-                    <TableCell align="center">{tahun}</TableCell>
+                    <TableCell align="center">
+                      <Stack direction="row" spacing={2}>
+                      <Button variant='outlined' color='error' size='small' startIcon={<DeleteIcon/>} 
+                      onClick={()=>{deleteDocument(k.id)}}
+                      >
+                        Hapus
+                      </Button>
+                      <Button variant='outlined' size='small' startIcon={<ModeIcon/>} 
+                      // onClick={()=>{deleteDocument(k.id)}}
+                      >
+                        Edit
+                      </Button>
+                      </Stack>
+                    </TableCell>
+                    {/* <TableCell align="center">{tahun}</TableCell>
                     <TableCell align="center">aksi</TableCell> */}
                 </TableRow>
               )})}
